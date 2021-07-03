71f74ffc04a6b7537030d6857c8d724cc59d2c76
Akred <akred@live.com>
2021-07-03 13:17:55 +0200

    Add note model

---
+++ src/models/note.d.ts
@@ -0,0 +1,20 @@
+/**
+ * We can use Type instead of Interface
+ * From what I read in documentation, The only extra feature Interfaces bring to the table (that Type aliases don’t),
+ * is “declaration merging” which means you can define the same interface several times and with each definition, the properties get merged
+ */
+interface INote {
+    _id: ID! // "unique ID of the note"
+    content: String! // content of the note
+}
+
+interface NoteProps {
+    note: INote
+}
+
+type ApiDataType = {
+    message: string
+    status: string
+    notes: INote[]
+    note?: INote
+}
