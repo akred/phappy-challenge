ee57f29caa5bfceb7cc203c31a1b734f40bd36c8
Akred <akred@live.com>
2021-07-11 17:48:13 +0200

    Display call detail

--- src/components/CallDetail.tsx
+++ src/components/CallDetail.tsx
@@ -1,7 +1,10 @@
 import LoadingIcon from "./icons/LoadingIcon";
 import CallService from "../services/CallService";
-import { useEffect, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { CALL_LIST_URL } from "../helpers/urls";
+import moment from "moment";
 
 export default function CallDetail() {
   const [call, setCall] = useState<ICall>();
@@ -26,13 +29,50 @@ export default function CallDetail() {
   useEffect(() => {
     fetchCall();
   }, []);
+
+  const history = useHistory();
+
+  const handleClick = (e: MouseEvent) => {
+    e.preventDefault();
+    history.push(CALL_LIST_URL);
+  };
   return (
     <div className="phappy-call-detail">
       <h1 className="title">Call detail</h1>
       {isLoading ? (
         <LoadingIcon />
       ) : !!call ? (
-        <h2>ID : {call.id}</h2>
+        <div>
+          <h2></h2>
+          <article className="message is-primary">
+            <div className="message-header">
+              <p>Call ID : {call.id}</p>
+              <button
+                className="delete"
+                aria-label="close"
+                onClick={handleClick}
+              ></button>
+            </div>
+            <div className="message-body content">
+              <ul>
+                <li>ID : {call.id}</li>
+                <li>Direction : {call.direction}</li>
+                <li>From : {call.from}</li>
+                <li>To : {call.to}</li>
+                <li>Duration : {moment(call.duration).format('H:mm:ss')}</li>
+                <li>Archived : {call.is_archived ? 'Yes' : 'No'}</li>
+                <li>Via : {call.via}</li>
+                <li>
+                  Created at :{" "}
+                  {moment(call.created_at.toString()).format("MMMM DD, h:mm A")}
+                </li>
+                <li>
+                  Notes : <i>todo</i>
+                </li>
+              </ul>
+            </div>
+          </article>
+        </div>
       ) : (
         <h2>No data available</h2>
       )}
