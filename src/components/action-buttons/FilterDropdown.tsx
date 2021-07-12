612bee249a972e1ed5cdabc1a55bd62fc0331fb9
Akred <akred@live.com>
2021-07-12 22:57:29 +0200

    Add filter dropdown (WIP)

---
+++ src/components/action-buttons/FilterDropdown.tsx
@@ -0,0 +1,63 @@
+import { MouseEvent, useState } from "react";
+import classNames from "classnames";
+import "./FilterDropdown.scss";
+type FilterDropdownProps = {
+  filterTypes: string[];
+  selectedFilter: string;
+  parentCallback: (selectedFilter: string) => void;
+};
+
+/**
+ * FilterDropdown
+ * This component is mainly used to filter calls by type (missed, answered, archived)
+ */
+const FilterDropdown = ({
+  filterTypes,
+  parentCallback,
+}: FilterDropdownProps) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  const handleClick = (e: MouseEvent) => {
+    e.preventDefault();
+    setIsClicked(!isClicked)
+  }
+  return (
+    <div
+      className={classNames("phappy-filter-dropdown dropdown", {
+        "is-active": isClicked
+      })}
+    >
+      <div className="dropdown-trigger">
+        <button
+          className="button"
+          aria-haspopup="true"
+          aria-controls="dropdown-menu3"
+          onClick={handleClick}
+        >
+          <span>Filter</span>
+          <span className="icon is-small">
+            <i className="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+        <div className="dropdown-content">
+          {filterTypes.map((value, index) => {
+            return (
+              <a
+                href="/#"
+                className="dropdown-item"
+                key={index}
+                onClick={() => parentCallback(value)}
+              >
+                {value}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterDropdown;
